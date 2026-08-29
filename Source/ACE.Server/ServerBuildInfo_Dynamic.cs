dcaabb139f9f5d649d597b49f5d5c7193e967a66
Danny Hutchins <drhutchins@hotmail.com>
2026-08-29 11:02:29 -0400

    Merge remote master into local master

--- Source/ACE.Server/ServerBuildInfo_Dynamic.cs
+++ Source/ACE.Server/ServerBuildInfo_Dynamic.cs
@@ -4,17 +4,17 @@ namespace ACE.Server
     public static partial class ServerBuildInfo 
     { 
         public static string Branch = "master"; 
-        public static string Commit = "c2ed2eab9a4f05ab07e65a58680daf3d3e2b3276"; 
+        public static string Commit = "1de1dc23e7a7983849f8d4fe412ddc8b7960655d"; 
  
         public static string Version = "1.77"; 
-        public static string Build   = "4797"; 
+        public static string Build   = "4801"; 
  
         public static int BuildYear   = 2026; 
         public static int BuildMonth  = 08; 
-        public static int BuildDay    = 03; 
-        public static int BuildHour   = 11; 
-        public static int BuildMinute = 49; 
-        public static int BuildSecond = 42; 
+        public static int BuildDay    = 13; 
+        public static int BuildHour   = 05; 
+        public static int BuildMinute = 13; 
+        public static int BuildSecond = 35; 
     } 
 } 
  
